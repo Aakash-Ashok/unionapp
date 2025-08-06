96409b91f900a7a717a7bc551cb6684d1a8bc860
Akash Ashok <akashask2012@gmail.com>
2025-08-06 21:31:33 +0530

    wsgi

--- unionapp/wsgi.py
+++ unionapp/wsgi.py
@@ -1,16 +1,15 @@
-"""
-WSGI config for unionapp project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
-"""
-
 import os
-
+import django
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'unionapp.settings')
 
 application = get_wsgi_application()
+
+# 🔥 Run migration on startup
+try:
+    django.setup()
+    from django.core.management import call_command
+    call_command('migrate', interactive=False)
+except Exception as e:
+    print("Migration error:", str(e))
