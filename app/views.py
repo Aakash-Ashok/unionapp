0f7d0db96d21dcbf221319c828357dd4e735d724
Akash Ashok <83403116+Aakash-Ashok@users.noreply.github.com>
2025-08-12 20:40:59 +0530

    Update viewsss.py

--- app/views.py
+++ app/views.py
@@ -14,14 +14,10 @@ from .serializers import (
 
 # ------------------- Announcement Views -------------------
 
-class ClubMemberListCreateAPIView(APIView):
+class AnnouncementListCreateAPIView(APIView):
     def get(self, request):
-        club_id = request.query_params.get('club')
-        if club_id:
-            members = ClubMember.objects.filter(club_id=club_id)
-        else:
-            members = ClubMember.objects.all()
-        serializer = ClubMemberSerializer(members, many=True)
+        announcements = Announcement.objects.all().order_by('-posted_on')
+        serializer = AnnouncementSerializer(announcements, many=True)
         return Response(serializer.data)
 
 
